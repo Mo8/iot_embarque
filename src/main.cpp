ef4702052606b64a62cc9a5cffec095a64ba8a75
Mo8 <juliancautela@gmail.com>
2023-06-20 16:27:25 +0200

    c marche sauf subscribe

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,29 @@
 #include <WiFi.h>
 #include <ArduinoJson.h>
 #include <PubSubClient.h>
+#include <EEPROM.h>
+#define addr_temp_freq 0
+#define addr_connection_freq 1
+#define addr_connection_config 2
+#define EEPROM_SIZE 12
 
 const char *ssid = "esp32_wifi";
 const char *password = "wifi1234";
 String serverip = "192.168.43.150:3000";
 RTC_DATA_ATTR byte tab_temp[50];
 RTC_DATA_ATTR byte counter = 0;
-RTC_DATA_ATTR byte tempFreq = 10;        // frequence lecture
-RTC_DATA_ATTR byte connectionFreq = 10;  // frequence envoie
-RTC_DATA_ATTR byte connectionConfig = 3; // frequence envoie
+byte tempFreq = 10;// frequence lecture
+byte connectionFreq = 30;// frequence envoie
+byte connectionConfig = 2;// frequence envoie
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+
+void printConfig(){
+  Serial.println(String("tf = ") + tempFreq);
+  Serial.println(String("cg = ") + connectionConfig);
+  Serial.println(String("cf = ") + connectionFreq);
+}
 
 void setup()
 {
@@ -29,6 +40,16 @@ void setup()
   // read CPU speed
   rtc_cpu_freq_config_t freq_config;
   rtc_clk_cpu_freq_get_config(&freq_config);
+//Init EEPROM
+  EEPROM.begin(EEPROM_SIZE);
+ byte t_tempFreq = EEPROM.read(addr_temp_freq) ;        // frequence lecture
+ byte t_connectionFreq = EEPROM.read(addr_connection_freq); // frequence envoie
+ byte t_connectionConfig = EEPROM.read(addr_connection_config);
+ 
+ tempFreq = t_tempFreq != 10 && t_tempFreq != 20 && t_tempFreq != 60 ? 10 : t_tempFreq;
+ connectionFreq = t_connectionFreq != 20 && t_connectionFreq != 30 && t_connectionFreq != 180 ? 20 : t_connectionFreq;
+ connectionConfig = t_connectionConfig != 1 && t_connectionConfig != 2 && t_connectionConfig != 3 ? 2 : t_connectionConfig;
+ printConfig();
 }
 
 unsigned long getTime()
@@ -60,26 +81,32 @@ void initWifi()
   Serial.println(WiFi.localIP());
 }
 
-void updateConfig(StaticJsonDocument<512> doc){
+void updateConfig(String payload)
+{
+  StaticJsonDocument<512> doc;
+  DeserializationError error = deserializeJson(doc, payload);
+  if (error)
+  {
+    Serial.println(F("Failed to read file, using default configuration"));
+    return;
+  }
+  tempFreq = doc["tempFreq"];
+  EEPROM.put(addr_temp_freq, tempFreq);
 
-    tempFreq = doc["tempFreq"];
+  connectionFreq = doc["connectionFreq"];
+  EEPROM.put(addr_connection_freq, connectionFreq);
 
-    connectionFreq = doc["connectionFreq"];
+  connectionConfig = doc["connectionConfig"];
+  EEPROM.put(addr_connection_config, connectionConfig);
 
-    connectionConfig = doc["connectionConfig"];
+  EEPROM.commit();
+  printConfig();
 
-    Serial.println(String("tf = ") + tempFreq);
-    Serial.println(String("cg = ") + connectionConfig);
-    Serial.println(String("cf = ") + connectionFreq);
+  
 }
 
-
-void sendDataHttp()
+String generateJsonData()
 {
-  HTTPClient http;
-  String url = "http://" + serverip + "/host/api/Esp32/oui";
-  http.begin(url.c_str());
-  http.addHeader("Content-Type", "application/json");
   String payload = "{\"config\":{\"tempFreq\":" + String(tempFreq) + ",\"connectionConfig\":" + String(connectionConfig) + ",\"connectionFreq\":" + String(connectionFreq) + "},\"temperatures\":[";
   for (int i = 0; i < counter; i++)
   {
@@ -89,6 +116,16 @@ void sendDataHttp()
       payload += ",";
   }
   payload += "]}";
+  return payload;
+}
+
+void sendDataHttp()
+{
+  HTTPClient http;
+  String url = "http://" + serverip + "/host/api/Esp32/oui";
+  http.begin(url.c_str());
+  http.addHeader("Content-Type", "application/json");
+  String payload = generateJsonData();
   int res = http.PUT(payload);
 
   if (res > 0)
@@ -97,14 +134,8 @@ void sendDataHttp()
     Serial.println(res);
     String payload = http.getString();
     Serial.println(payload);
-    StaticJsonDocument<512> doc;
-    DeserializationError error = deserializeJson(doc, payload);
-    if (error)
-    {
-      Serial.println(F("Failed to read file, using default configuration"));
-      return;
-    }
-    updateConfig(doc);
+
+    updateConfig(payload);
   }
   else
   {
@@ -115,48 +146,48 @@ void sendDataHttp()
   http.end();
 }
 
-void callback(char* topic, byte* message, unsigned int length) {
+void callback(char *topic, byte *message, unsigned int length)
+{
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
-  
-  for (int i = 0; i < length; i++) {
+
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
   Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
-  if (String(topic) == "/ynov/esp32-CAUTELA/out") {
-    Serial.print("Changing output to ");
-    if(messageTemp == "on"){
-      Serial.println("on");
-     // digitalWrite(ledPin, HIGH);
-    }
-    else if(messageTemp == "off"){
-      Serial.println("off");
-      //digitalWrite(ledPin, LOW);
-    }
+  if (String(topic) == "/ynov/esp32-CAUTELA/out")
+  {
+    updateConfig(messageTemp);
   }
 }
 
-void sendDataMqtt(){
-  
+void sendDataMqtt()
+{
+
   client.setServer("broker.hivemq.com", 1883);
   client.setCallback(callback);
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("my_idjkhjkgkjghjgh656456465")) {
+    if (client.connect("my_idjkhjkgkjghjgh656456465"))
+    {
       Serial.println("connected");
       // Subscribe
-      client.subscribe("/ynov/esp32-CAUTELA/out");
-      client.publish("/ynov/esp32-CAUTELA/in","ajdskjskdfjsjldjf");
-    } else {
+      Serial.println(client.subscribe("/ynov/esp32-CAUT/out"));
+      delay(400);
+
+      client.publish("/ynov/esp32-CAUT/in", generateJsonData().c_str());
+      delay(1000);
+      client.loop();
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -164,14 +195,14 @@ void sendDataMqtt(){
       delay(5000);
     }
   }
-
-  
 }
-
 
 void loop()
 {
+
+  esp_sleep_wakeup_cause_t cause = esp_sleep_get_wakeup_cause();
   byte temp2 = readTemp2(false);
+
   // unsigned long time = getTime();
   Serial.printf("Temp2 : %u, Wifi status %s\n Compteur : %u\n", temp2, WiFi.status() == WL_CONNECTED ? "connected" : "disconnected", counter);
   tab_temp[counter] = temp2;
@@ -181,8 +212,11 @@ void loop()
   {
     Serial.println("Sending data");
     initWifi();
-    //sendDataHttp();
-    sendDataMqtt();
+    if (connectionConfig == 1 || connectionConfig == 3)
+      sendDataMqtt();
+    if (connectionConfig == 2 || connectionConfig == 3)
+      sendDataHttp();
+
     counter = 0;
   }
   Serial.flush();
